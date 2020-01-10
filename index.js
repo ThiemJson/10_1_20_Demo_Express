4572d94a0adf34981210bff90559d7bec9215e81
thiemjson <thiemjson@gmail.com>
2020-01-11 00:46:52 +0700

    asdas

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express();
-const port = 3000;
+const port = 2000;
 
 app.listen(port,()=>{
     console.log(`Server listen on port `+port);
 });
 
 app.get('/',(request,response)=>{
-    response.send("Hellooooo");
+    response.send("Hellooooo somethhhing");
+});
+
+app.get('/users',(request,response)=>{
+    response.send("dasdasd");
 });
