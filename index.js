2fe6dde713d641d723e0093c157170209cf150dc
thiemjson <thiemjson@gmail.com>
2020-01-11 21:27:39 +0700

    asdasd

--- index.js
+++ index.js
@@ -5,17 +5,23 @@ const port = 2000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+
+users = [
+    {id: 1, name: "Thiem"},
+    {id: 2, name: "Chou"},
+    {id: 3, name: "Thiem"},
+    {id: 1, name: "Thiem"},
+]
 app.listen(port,()=>{
     console.log(`Server listen on port `+port);
 });
 
+app.get('/users/search',(request,response)=>{
+    console.log(request.query);
+});
+
 app.get('/users',(request,response)=>{
     response.render("users/index.pug",{
-        users:[
-            {id: 1, name: "Thiem"},
-            {id: 2, name: "Chou"},
-            {id: 3, name: "Thiem"},
-            {id: 1, name: "Thiem"},
-        ]
+        users: users
     });
 });
